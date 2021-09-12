b9a757a1de68d8a852c864d3176bafd5b09b5376
ardeshir <ardeshir.ahouri@gmail.com>
2021-09-12 17:25:41 +0430

    initial commit

---
+++ src/main/kotlin/com/ardeshir/tddmockitokotlin/TddMockitoKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.ardeshir.tddmockitokotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TddMockitoKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<TddMockitoKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/ardeshir/tddmockitokotlin/TddMockitoKotlinApplicationTests.kt
@@ -0,0 +1,8 @@
+package com.ardeshir.tddmockitokotlin
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TddMockitoKotlinApplicationTests {
+
+}
